94867c3a9ab64dbdfd1a76804d63fdc88046f70e
Abhay <itsabhayanand@gmail.com>
2025-05-26 15:05:44 +0530

    Update login.jsx

--- frontend/src/Pages/login.jsx
+++ frontend/src/Pages/login.jsx
@@ -35,7 +35,6 @@ const Login = () => {
   const FacebookLogin = async () => {
     window.location.href = "http://localhost:5001/auth/facebook";
   }
-
   //Github login
   const GithubLogin = async () => {
     window.location.href = "http://localhost:5001/auth/github";
